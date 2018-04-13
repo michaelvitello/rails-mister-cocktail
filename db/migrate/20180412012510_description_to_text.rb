ee641014be58b44a2a1f31f8ef43f413517733eb
michael vitello <michael.vitello@gmail.com>
2018-04-13 12:24:34 +1000

    2nd Try

--- db/migrate/20180412012510_description_to_text.rb
+++
@@ -1,5 +0,0 @@
-class DescriptionToText < ActiveRecord::Migration[5.1]
-  def change
-    change_column :doses, :description, :text
-  end
-end
--- db/migrate/20180412020005_ingredient_id_as_ref.rb
+++
@@ -1,5 +0,0 @@
-class IngredientIdAsRef < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :doses, :ingredient_id, index: true
-  end
-end
